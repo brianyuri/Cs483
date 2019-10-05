0563ac610f2929f21d91bc228c89182dfff012b3
Brian Yuri Fachini <brianyurif17@gmail.com>
2019-10-05 19:27:57 -0300

    Aula 05/10

---
+++ Cs483/Cs483.ConsoleApp/Aula1/6-Enum/Enums.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cs483.ConsoleApp.Aula1._6_Enum
+{
+    class Enums
+    {       
+        public void Executa()
+        {
+            Console.WriteLine("Enuns");
+            DiasDaSemana mtDt = DiasDaSemana.Segunda | DiasDaSemana.Terca | DiasDaSemana.Quarta;
+            Console.WriteLine(mtDt);
+        }
+        [Flags]
+        
+        enum DiasDaSemana
+        {
+            Segunda = 1,
+            Terca = 2,
+            Quarta = 4,
+            Quinta = 8,
+            Sexta = 16,
+            Sabado = 32,
+            Domingo = 64,
+    }
+    }
+}
